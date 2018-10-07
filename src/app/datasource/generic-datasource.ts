c62f1f45096f193d52624cc983abe78f362d4180
kakusharma <gauravsharma230394@gmail.com>
2018-10-07 15:27:30 +0530

    Unsubscribe the subject and added default control in datatable

--- src/app/datasource/generic-datasource.ts
+++ src/app/datasource/generic-datasource.ts
@@ -1,11 +1,13 @@
 import { DataSource } from '@angular/cdk/table';
 import { CollectionViewer } from '@angular/cdk/collections';
-import { Observable, BehaviorSubject } from 'rxjs';
+import { Observable, BehaviorSubject, Subscription } from 'rxjs';
 import { DataService } from '../services/data.service';
 import { map } from 'rxjs/operators';
+import { OnDestroy } from '@angular/core';
 
-export class GenericDatasource<T> implements DataSource<T> {
+export class GenericDatasource<T> implements DataSource<T>, OnDestroy {
     private subject = new BehaviorSubject<T[]>([]);
+    dataSubscription: Subscription;
     constructor(private dataService: DataService<T>) { }
     connect(collectionViewer: CollectionViewer): Observable<T[]> {
         return this.subject.asObservable();
@@ -13,12 +15,15 @@ export class GenericDatasource<T> implements DataSource<T> {
     disconnect(collectionViewer: CollectionViewer): void {
         this.subject.complete();
     }
+    ngOnDestroy() {
+        this.dataSubscription.unsubscribe();
+    }
     initDataSource(url: string) {
-        this.dataService.getData(url)
+        this.dataSubscription = this.dataService.getData(url)
             .subscribe(
                 (data: T[]) => {
-                   this.subject.next(data);
-                  }
+                    this.subject.next(data);
+                }
             );
     }
 }
