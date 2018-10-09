f77c2da38901273818f5557f50c3b1f5ad70d1db
Gaurav Sharma <GauravSh@talentica-all.com>
2018-10-09 19:42:48 +0530

    Added angular material label control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   modelMetadata = [
     { propertyName: 'userId', displayText: 'User Id', control: 'select', order: 2, options: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]},
-    { propertyName: 'id', displayText: 'Id', order: 1, options: []},
+    { propertyName: 'id', displayText: 'Id', control: 'label', order: 1, options: []},
     { propertyName: 'title', displayText: 'Title', control: 'textbox', order: 3, options: []},
     { propertyName: 'completed', displayText: 'Completed', control: 'checkbox', order: 4, options: []}
   ];
